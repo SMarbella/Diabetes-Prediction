05822459ede1b9406f8a30f4984276919fcd7f85
SMarbella <SWMarbella@arizona.edu>
2023-07-30 22:40:18 -0700

    Added required files.

---
+++ Diabetes Charts.R
@@ -0,0 +1,11 @@
+if(!require(pacman))install.packages("pacman")
+
+pacman::p_load('dplyr', 'tidyr', 'gapminder',
+               'ggplot2',  'ggalt',
+               'forcats', 'R.utils', 'png', 
+               'grid', 'ggpubr', 'scales',
+               'bbplot')
+
+library(ggplot2)
+library(datasets)
+library(help = "datasets")
